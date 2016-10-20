babe857b228f9cc11b3dc8d89c955b413154d422
prasaadem <prasaadem@gmail.com>
2016-10-20 00:43:45 -0500

    Push to GitHub
    This is my first attempt to push all my code to GitHub

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+#Made changes here  
+  	setup = Movie.set_options(params, session)
+  	
+    @ratings = Movie.ratings
+    @filters = setup[:ratings]
+    @movies = Movie.movies(@filters, setup[:order_by])
+    
+    session[:ratings] = setup[:ratings]
+    session[:order_by] = setup[:order_by]
+    
+    
   end
 
   def new
